5989a90344aa7fa8d61c340a5b021191ae0a373a
hiro8190 <kdr.8190@gmail.com>
2019-12-09 19:39:22 +0900

    create registration view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   def index
-  
+    
   end
 
+  def step1
+  end
+
+  def step2
+  end
+
+  def step3
+  end
+
+  def step4
+  end
+
+  def step5
+  end
   
 end
